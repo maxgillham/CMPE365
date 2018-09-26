7d003877f1e898a8988f1123af31bca55cd85f87
Max Gillham <14mfg2@queensu.ca>
2018-09-26 16:24:29 -0400

    question 1 working

--- lab2/mss.py
+++ lab2/mss.py
@@ -1,3 +1,35 @@
+
+'''
+This method accepts a list, left and right indicies, a current greatest (greatest sum value), current_sub_list (the sub
+list corresponding to the current greatest sum.  The method then returns the greatest segment sum and the corresponding
+segment
+'''
+def recurse_by_endpoints(list, left_index, right_index, current_greatest, current_sub_list):
+    if right_index >= left_index:
+        i = 0
+        while i < len(list) - right_index:
+            temp_sum = get_sum(list[i:right_index+i+1])
+            if temp_sum > current_greatest:
+                current_greatest = temp_sum
+                current_sub_list = list[i:right_index+i+1]
+                print('new greatest is: ', current_greatest, 'for sublist: ', list[i:right_index+i+1])
+            i += 1
+        recurse_by_endpoints(list, left_index, right_index - 1, current_greatest, current_sub_list)
+    else:
+        return current_greatest, current_sub_list
+
+def get_sum(list):
+    return sum(list)
+
+if __name__ == '__main__':
+    test_list = [2, -4, 5, -1, 3, -6, -1, 4]
+    recurse_by_endpoints(list=test_list, left_index=0, right_index=len(test_list), current_greatest=get_sum(test_list), current_sub_list=test_list)
+
+
+
+
+#first attempt.  Didn't check for middle cases.  Saving for later.
+'''
 import math
 
 def question_1(list, left, right):
@@ -16,12 +48,4 @@ def recurse(list, sum_left, sum_right):
             recurse(list[:divide_index], sum_left, sum_right)
             recurse(list[divide_index:], sum_left, sum_right)
     return sum_left, sum_right
-
-def get_sum(list):
-    return sum(list)
-
-if __name__ == '__main__':
-    test_list = [2, 4, 5, -1, 3, -6, 1, 4]
-    print('initial', test_list)
-    sum_left, sum_right = question_1(test_list, 0, 8)
-    
+'''
