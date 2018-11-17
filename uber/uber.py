e9e5ea5ca00fcef0a24e5ba274530a0b3e04770e
Max Gillham <14mfg2@queensu.ca>
2018-11-17 13:16:41 -0500

    added list of driver instances

--- uber/uber.py
+++ uber/uber.py
@@ -1,26 +1,58 @@
 
 import numpy as np
 import itertools
+import matplotlib.pyplot as plt
 
+def wait_times_n_drivers(n, network, requests):
+    
+    #init wait count for all drivers to be zero
+    total_wait_time = 0
+    #create a list of instances of driver class
+    drivers = []
+    #create n instances of drivers
+    for i in range(n):
+        drivers.append(Driver(network_map=network))
 
-
-def wait_times(network, requests):
-    #init 2 drivers starting at location 0 at time of first request
-    driver_1 = Driver(network_map=network)
-    driver_2 = Driver(network_map=network)
-
+    #loop through all requests
     for i in range(requests.shape[0]):
-
+        
+        #set request variables for ease of use
         request_time = requests[i,0]
         pickup_location = int(requests[i,1])-1
         dropoff_location = int(requests[i,2])-1
 
-        print('\nRequest at time', request_time)
-        print('~~~')
-        print('Driver 1 Time', driver_1.time, 'At Location', driver_1.location)
-        print('With ETA', driver_1.get_eta(pickup_location))
-        print('\nDriver 2 Time', driver_2.time, 'At Location', driver_2.location)
-        print('With ETA', driver_2.get_eta(pickup_location),'\n')
+        #update time to request time for all drivers
+        for i in range(n): drivers[i].update_time(request_time)
+        
+        #create a empty array of zeros
+        eta = np.zeros(n)
+        #update driver eta to pickup for each driver
+        for i in range(n): eta[i] = drivers[i].get_eta(pickup_location)
+
+        #find driver with earliest eta
+        driver_index = np.argmin(eta)
+
+        #get driver with earliest eta to pickup and dropoff
+        drivers[driver_index].pickup(pickup_location=pickup_location, request_time=request_time)
+        drivers[driver_index].dropoff(dropoff_location=dropoff_location)
+
+    #count late times for each driver in request
+    for i in range(n): total_wait_time += drivers[i].late_count
+    #return total wait time
+    return total_wait_time
+
+def wait_times_2_drivers(network, requests):
+    #init 2 drivers starting at location 0 at time of first request
+    driver_1 = Driver(network_map=network)
+    driver_2 = Driver(network_map=network)
+
+    #loop through requests
+    for i in range(requests.shape[0]):
+
+        #set variables for ease of use
+        request_time = requests[i,0]
+        pickup_location = int(requests[i,1])-1
+        dropoff_location = int(requests[i,2])-1
 
         #update drivers to time of request if they at prev time
         #if they are seeing request after its been made, ignore
@@ -36,9 +68,33 @@ def wait_times(network, requests):
             driver_2.pickup(pickup_location=pickup_location, request_time=request_time)
             driver_2.dropoff(dropoff_location=dropoff_location)
 
-        print('\ndriver 1 late count', driver_1.late_count)
-        print('driver 2 late count', driver_2.late_count)
+    #return sum of driver 1 and driver 2 late counts
     return driver_1.late_count + driver_2.late_count
+
+def wait_times_1_driver(network, requests):
+    
+    #create instance of driver class
+    driver = Driver(network_map=network)
+
+    #iterate through all requests
+    for i in range(requests.shape[0]):
+
+        #set variables for ease of use
+        request_time = requests[i,0]
+        pickup_location = int(requests[i,1])-1
+        dropoff_location = int(requests[i,2])-1
+
+        #update driver time to request time
+        driver.update_time(request_time)
+
+        #pickup requestor
+        driver.pickup(pickup_location, request_time)
+        #dropoff requestor
+        driver.dropoff(dropoff_location)
+    
+    #return late count for single driver
+    return driver.late_count
+
 
 #driver class
 class Driver:
@@ -78,6 +134,18 @@ def floyd_warshall(city_graph):
         if city_graph[i,j] > city_graph[i,k] + city_graph[k,j]: city_graph[i,j] = city_graph[i,k] + city_graph[k,j]
     return city_graph
 
+#plotting method
+def plot_wait_times(city_graph, requests, range_of_drivers):
+    wait_times = []
+    for i in range(1,range_of_drivers):
+        wait_times.append(wait_times_n_drivers(i, city_graph, requests))
+    plt.scatter(np.arange(1,range_of_drivers), wait_times, marker='o', c='c')
+    plt.xlabel('Number of Drivers')
+    plt.ylabel('Total Time Spent Waiting')
+    plt.title('Time Spent Waiting for 100 Given Drivers')
+    plt.show()
+    return
+
 #just a csv open to numpy
 def load_csv(fname):
     return np.loadtxt(open(fname, 'rb'), delimiter=',')
@@ -92,5 +160,6 @@ if __name__ == '__main__':
     #optimize city graph
     city_graph = floyd_warshall(city_graph)
 
-    late_count = wait_times(city_graph, requests)
+    #plot_wait_times(city_graph, requests, 100)
+    late_count = wait_times_n_drivers(10,city_graph, requests)
     print(late_count)
